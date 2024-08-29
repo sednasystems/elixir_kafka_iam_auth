8b41c96f0c3b5e8a16889735cc35369e50965ab7
Nigel Smith <nigel.j.smith@gmail.com>
2024-08-29 17:25:01 +0100

    Bump versions

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ExAwsMskIamAuth.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:broadway_kafka, "~> 0.3.5"},
+      {:broadway_kafka, "~> 0.4.1"},
       {:aws_signature, "~> 0.3.0"},
       {:jason, "~> 1.3"},
       {:hammox, "~> 0.5", only: :test}
